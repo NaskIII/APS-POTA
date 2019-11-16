7b461dfb982dd799469eda5dc07cd6304b50cc86
Raphael Nask <raphael_nask@hotmail.com>
2019-11-16 00:07:47 -0300

    Sorts implementados

--- Python/Main.py
+++ Python/Main.py
@@ -1,9 +1,12 @@
-import Array.Array
+from Array import Array
+from Sorts import Sorts
 
 
 class Main(object):
     if __name__ == '__main__':
-        arr = Array.Array.Array()
+        arr = Array.Array()
+        sort = Sorts.Sort()
 
         vetor = arr.getArray(10)
+        sort.radixSort(vetor)
         print(vetor)
